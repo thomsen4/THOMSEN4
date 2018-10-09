de7866dba2b6298d3288fc7ad22bf51d52844639
thomsen4 <43766624+thomsen4@users.noreply.github.com>
2018-10-08 19:54:27 -0500

    Final for 10/8/18

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var config = require('./config/config');
+
 var app = express();
-require('./config/express')(app, config.port);
-require('http').createServer(app).listen(port, function () {
-console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
+
+require('./config/express')(app, config);
+
+require('http').createServer(app).listen(config.port, function () {
+    console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
